18f1f109405e7027d71ccbd4e6a0a2db646db67a
seag86 <seag86@gmail.com>
2021-11-12 17:28:44 +0300

    somechanges

--- app/screens/tags/tags.tsx
+++ app/screens/tags/tags.tsx
@@ -2,7 +2,7 @@ import React, { FC, useEffect, useState } from "react"
 import {
   ImageStyle, Platform, TextStyle, View, ViewStyle,
   TouchableOpacity,
-  FlatList,
+  ScrollView,
   Text as RNText
 } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
@@ -20,6 +20,7 @@ import {
 } from "../../components"
 import { NavigatorParamList, navigate } from "../../navigators"
 import { color, spacing, style as s } from "../../theme"
+import { PhotoStoreModel } from "../../models/photo-store/photo-store"
 
 
 const CONTAINER: ViewStyle = {
@@ -27,20 +28,24 @@ const CONTAINER: ViewStyle = {
   marginTop: spacing[4],
   backgroundColor: color.transparent,
   paddingHorizontal: spacing[4],
-  flexDirection:'row',
-  flexWrap:'wrap'
+  flexDirection: 'row',
+  flexWrap: 'wrap'
 }
 
-const LOVE: TextStyle = {
-  color: "#BAB6C8",
-  fontSize: 15,
-  lineHeight: 22,
+const SCROLL: ViewStyle = {
+  flex: 1,
+  marginTop: spacing[4],
+  backgroundColor: color.transparent,
+  paddingHorizontal: spacing[4],
+  flexDirection: 'row',
+  flexWrap: 'wrap'
 }
+
 
 const BOLD: TextStyle = { fontWeight: "bold" }
 
 
-export const TagsScr: FC<StackScreenProps<NavigatorParamList, "Tags">> = observer(
+export const Tags: FC<StackScreenProps<NavigatorParamList, "Tags">> = observer(
   ({ navigation, route }) => {
     const goBack = () => navigation.goBack()
 
@@ -52,8 +57,14 @@ export const TagsScr: FC<StackScreenProps<NavigatorParamList, "Tags">> = observe
 
     const fetchMany = async () => {
       const page = 0
-      const limit = 100
+      const limit = 30
       await photoStore.getPhotos(page, limit)
+    }
+
+    const tagPressHandle = async (tag) => {
+      //navigate('Home', { tag: tag })
+      await photoStore.savePhotos(tag.photos)
+      console.log('photos', photos)
     }
 
     useEffect(() => {
@@ -65,15 +76,13 @@ export const TagsScr: FC<StackScreenProps<NavigatorParamList, "Tags">> = observe
     }, [photos])
 
     const sortAuthors = (d: Array<any>) => {
-
-      //const tags: Array<Tag>
       const authors = d.reduce((acc, element) => {
         const [key, Name] = Object.entries(element)[1];
         (acc[Name] || (acc[Name] = [])).push(element);
         return acc;
       }, {});
 
-      const _tags = []
+      const _tags: Array<any> = []
       for (const author in authors) {
         _tags.push({
           author: author,
@@ -83,34 +92,30 @@ export const TagsScr: FC<StackScreenProps<NavigatorParamList, "Tags">> = observe
       setTags(_tags)
     }
 
-    const tagPressHandle = (tag) => {
-      //console.log('tags', authors[tag])
-      //tags
 
-    }
 
     return (
-      <Screen testID="Tags" style={CONTAINER}>
+      <Screen testID="Tags" style={CONTAINER} isNonScrolling={false}>
 
         {/* <RNText> {JSON.stringify(tags)}</RNText> */}
 
         {
           tags.length
             ?
-              tags.map((tag, index) => {
-                return (
-                  <View style={[s.row, s.smallBMargin]}>
-                    <TouchableOpacity style={[s.tag, s.mr5, s.mb10 ]}
-                    //onPress={() => navigate('Tags', { author: tag?.author })}
-                    >
-                      <RNText style={[]}>
-                        {tag?.author}
-                      </RNText>
-                    </TouchableOpacity>
-                    <View />
-                  </View>
-                )
-              })
+            tags.map((tag, index) => {
+              return (
+                <View style={[s.row, s.smallBMargin]} key={index}>
+                  <TouchableOpacity style={[s.tag, s.mr5, s.mb10]}
+                    onPress={() => tagPressHandle(tag)}
+                  >
+                    <RNText style={[]}>
+                      {tag?.author}
+                    </RNText>
+                  </TouchableOpacity>
+                  <View />
+                </View>
+              )
+            })
 
             : <RNText style={[]}>{'Loading...'}</RNText>
         }
